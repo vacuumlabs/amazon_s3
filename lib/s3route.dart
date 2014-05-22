357e371cc97c81cdaa073e1f003c17d9d4b7e31b
Marcela Hrda <aklecram@gmail.com>
2014-05-22 16:11:30 +0200

    Comment added.

--- lib/s3route.dart
+++ lib/s3route.dart
@@ -3,5 +3,13 @@ library amazon_s3.s3route;
 import 'package:clean_router/common.dart';
 
 class S3Route extends Route {
+  /**
+   * It would be nice to use url.join from path package here. However, it makes
+   * more harm than good, as it may lead to very unexpected results.
+   * For example:
+   *     url.join('http://google.com', 'a', 'b')  // -> http://google.com/a/b
+   *     url.join('http://google.com', 'a', '/b') // -> http://google.com/b
+   *     url.join('google.com', 'a', '/b')        // -> /b
+   */
   S3Route(host, bucket, path) : super('$host/$bucket/$path');
 }
