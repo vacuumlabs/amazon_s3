0e472043e34b0624d51b9cef6fdc6678b5588bf7
Marcela Hrda <aklecram@gmail.com>
2014-05-22 19:44:14 +0200

    bug fix

--- lib/s3route.dart
+++ lib/s3route.dart
@@ -4,5 +4,5 @@ import 'package:clean_router/common.dart';
 import 'package:path/path.dart';
 
 class S3Route extends Route {
-  S3Route(host, bucket, path) : super(url.join(host, bucket, path);
+  S3Route(host, bucket, path) : super(url.join(host, bucket, path));
 }
